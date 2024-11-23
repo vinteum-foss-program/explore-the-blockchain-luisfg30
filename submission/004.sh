881044e2f765c1d7c7cb05f0f91ebdc84999d691
Luis Guerreiro <luisfg3092@gmail.com>
2024-11-23 16:40:22 -0300

    Fixing question 4 output format

--- submission/004.sh
+++ submission/004.sh
@@ -3,4 +3,4 @@
 
 descriptor=$(bitcoin-cli getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/100)" | jq -r ."descriptor")
 
-bitcoin-cli deriveaddresses "$descriptor"
+bitcoin-cli deriveaddresses "$descriptor" | jq -r '.[0]'
